ba1b0adaa7e486bc4e0cef3996da46add24f6f4f
Johnny Ramirez <ramirezjohnny@icloud.com>
2021-07-09 11:09:47 -0400

    Add BB Letter Day API to list schedule for the entire year

--- controllers/scheduler.js
+++ controllers/scheduler.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+
 const parseLetterDays = require('../helpers/scheduler/parseLetterDays');
 const formatCourseTitle = require('../helpers/scheduler/formatCourseTitle');
 const formatSchedule = require('../helpers/scheduler/formatSchedule');
@@ -6,7 +8,7 @@ module.exports.renderScheduler = (req, res) => {
     return res.render('scheduler');
 }
 
-module.exports.uploadSchedule = (req, res) => {
+module.exports.uploadSchedule = async (req, res) => {
     if (!req.body.schedule) {
         return res.status(500).error('No schedule');
     }
@@ -20,19 +22,38 @@ module.exports.uploadSchedule = (req, res) => {
         const letterDays = parseLetterDays(courseInfo[0]);
         const period = parseInt(courseInfo[1]);
         const classRoom = courseInfo[2];
-        const courseTile = formatCourseTitle(courseInfo[4]);
+        const courseTitle = formatCourseTitle(courseInfo[4]);
         const teacher = courseInfo[6];
 
         return {
             letterDays,
             period,
             classRoom,
-            courseTile,
+            courseTitle,
             teacher
         };
     });
     
     const formattedSchedule = formatSchedule(scheduleObject);
 
-    return res.status(201).send({ formattedSchedule });
+    const letterDays = [];
+
+    await axios.get('https://www.blindbrook.org/Generator/TokenGenerator.ashx/ProcessRequest')
+    .then(tokenResponse => {
+        const config = {
+            headers: { Authorization: `Bearer ${tokenResponse.data.Token}` }
+        };
+        axios.get(
+            'https://awsapieast1-prod2.schoolwires.com/REST/api/v4/CalendarEvents/GetEvents/1009?StartDate=2020-09-01&EndDate=2021-06-30&ModuleInstanceFilter=&CategoryFilter=&IsDBStreamAndShowAll=true',
+            config        
+        )
+        .then(response => {
+            const data = response.data;
+            data.forEach(day => {
+                letterDays.push(day.Title);
+            });
+            return res.status(201).send({ schedule: formattedSchedule, letterDays });
+        });
+    });
+    return res.status(500).send({ error: 'was not able to load request' });
 }
--- public/javascripts/scheduler.js
+++ public/javascripts/scheduler.js
@@ -9,6 +9,24 @@ document.querySelector('button').addEventListener('click', () => {
     })
     .then(response => response.json())
     .then(response => {
-        console.log(response);
+        const { letterDays } = response;
+        letterDays.forEach(letterDay => {
+            const letter = letterDay[0];
+            const schedule = response.schedule[letter];
+            const h1 = document.createElement('h1');
+            h1.textContent = letterDay;
+            document.body.appendChild(h1);
+            if (!schedule) {
+                return;
+            }
+            schedule.forEach(course => {
+                const { period, courseTitle, classRoom, teacher } = course.course;
+                const { block, from, to } = course.time;
+                const p = document.createElement('p');
+                p.textContent = `${period}: ${courseTitle}, ${teacher}, ${classRoom}`;
+                document.body.appendChild(p);
+            });
+        });
+        
     });
 });
