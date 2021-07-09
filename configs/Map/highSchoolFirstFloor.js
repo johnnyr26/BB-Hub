ed12833f680502ba70f7a4284e9a8c883706bc16
Johnny Ramirez <ramirezjohnny@icloud.com>
2021-07-09 16:34:45 -0400

    Add path finding algorithm and adjust map
    Shows nearest direction when going from one location to another

--- configs/Map/highSchoolFirstFloor.js
+++ configs/Map/highSchoolFirstFloor.js
@@ -1,46 +1,37 @@
-const x = 'X';
-const f = 'FITT';
-const m = 'Middle School Cafeteria';
-const b = 'BandRoom';
-const y = 'LGI';
-const l = 'Library';
-const c = 'classroom';
-const n = 'nurse';
-const a = 'auditorium';
-const t = 'stage';
-const o = 'outdoors';
-const w = 'commons';
-const h = 'High School Gym';
-const g = 'Middle School Gym';
 
-const FIRST_FLOOR = 
-[ x, x, x, x, x, x, f, f, f, f, f, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x ],
-[ x, x, x, x, x, _, f, f, f, f, f, _, _, m, m, m, m, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x ],
-[ x, x, x, x, x, _, f, f, f, f, f, _, x, m, m, m, m, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x ],
-[ _, _, _, _, _, _,_,_,_,_,_,_, _, _, x, m, m, m, m, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x ],
-[ x, x, x, _, x, x, x, x, x, x, _, x, x, m, m, m, m, x, x, s, x, x, x, x, x, x, x, x, x, x, x, x, x ],
-[ c, c, c, _, x, h, h, h, h, h, _, x, x, x, x, _, _, _, _, _, x, x, x, x, x, x, x, x, x, x, x, x, x ],
-[ c, c, x, _, x, h, h, h, h, h, x, x, x, x, x, _, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x ],
-[ x, x, x, _, x, h, h, h, h, h, x, x, x, x, x, _, x, x, x, x, x, x, x, x, x, x, x, x, x, x, _, s, s ],
-[ c, c, _, _, x, h, h, h, h, h, x, x, x, x, x, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, o ]
-[ c, c, x, x, x, h, h, h, h, h, x, x, x, x, x, x, x, _, x, x, x, x, x, _, x, x, x, x, x, _, x, x, x ],
-[ x, x, x, x, x, h, h, h, h, h, x, x, x, x, x, x, x, _, w, x, b, b, b, b, b, x, x, x, x, _, _, c, c ],
-[ x, x, x, x, x, h, h, h, h, h, x, x, x, x, x ,x, x, _, w, x, b, b, b, b, b, x, x, x, x, _, x, c, c ], 
-[ x, x, x, x, x, h, h, h, h, h, x, x, x, x, x, x, x, _, w, x, b, b, b, b, b, x, x, x, x, _, x, x, x ],
-[ x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, _, w, x, x, x, x, x, _, x, x, x, x, _, _, c, c ],
-[ x, x, x, x, _, _, _, _, _, _, _, _, _, _, _, _, _, _, w, x, y, y, y, y, _, _, x, x, x, _, x, x, x ],
-[ x, x, x, x, x, x, x, x, s, s, x, _, _, _, _, _, _, _, w, x, y, y, y, y, x, _, _, _, _, _, _, c, c ],
-[ x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, _, w, x, y, y, y, y, x, c, x, c, x, _, x, x, x ],
-[ g, g, g, g, g, g, x, x, l, l, l, l, l, l, l, x, x, _, w, x, y, y, y, y, x, c, x, c, x, _, x, c, c ],
-[ g, g, g, g, g, g, x, x, l, l, l, l, l, l, l, x, c, _, x, x, x, x, x, _, x, _, x, _, x, _, x, _, x ],  
-[ g, g, g, g, g, g, x, x, l, l, l, l, l, l, l, x, x, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, s ],
-[ g, g, g, g, g, g, x, x, l, l, l, l, l, l, l, x, x, x, _, x, x, x, x, x, x, x, x, x, x, x, x, x, x ],
-[ g, g, g, g, g, g, x, x, l, l, l, l, l, l, l, x, x, x, _, s, x, x, x, x, x, x, x, x, x, x, x, x, x ], 
-[ g, g, g, g, g, g, x, x, l, l, l, l, l, l, l, l, l, x, _, x, x, x, x, x, x, x, x, x, x, _, x, x, x ],
-[ x, x, x, _, x, x, x ,x, x, x, _, x, x, x, x, x, x, x, _, x, n, n, n, x, a, a, a, a, a, a, x, t, t ],
-[ _, _, _, _, x, c, c, c, x, x, _, x, x, x, x, x, x, x, _, x, _, x, x, x, a, a, a, a, a, a, x, t, t ],
-[ x, x, x, _, x, _, x, x, x, x, _, _, _, _, _, _, _, _, _, _, _, _, _, _, a, a, a, a, a, a, x, t, t ],
-[ c, c, x, _, _, _, _, _, _, _, _, x, x, x, _, _, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x ],
-[ c, c, _, _, x, x, x, x, x, x, x, x, x, x, _, _, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x ],
-[ x, x, x, x, x, x, x, x, x, x, x, x, x, x, o, o, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x ];
 
+// TODO
+const FIRST_FLOOR = [
+[ 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'FITT', 'FITT', 'FITT', 'FITT', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL' ],
+[ 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'PATH', 'FITT', 'FITT', 'FITT', 'FITT', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'MCAF', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL' ],
+[ 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'PATH', 'WALL', 'PATH', 'WALL', 'WALL', 'WALL', 'PATH', 'WALL', 'MCAF', 'MCAF', 'PATH', 'MCAF', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL' ],
+[ 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'WALL', 'MCAF', 'MCAF', 'PATH', 'MCAF', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL' ],
+[ 'WALL', 'WALL', 'WALL', 'PATH', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'PATH', 'WALL', 'WALL', 'MCAF', 'MCAF', 'PATH', 'MCAF', 'WALL', 'WALL', 'STAI', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL' ],
+[ 'CLAS', 'CLAS', 'CLAS', 'PATH', 'WALL', 'HGYM', 'HGYM', 'HGYM', 'HGYM', 'HGYM', 'PATH', 'WALL', 'WALL', 'WALL', 'WALL', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL' ],
+[ 'CLAS', 'CLAS', 'WALL', 'PATH', 'WALL', 'HGYM', 'HGYM', 'HGYM', 'HGYM', 'HGYM', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'PATH', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL' ],
+[ 'WALL', 'WALL', 'WALL', 'PATH', 'WALL', 'HGYM', 'HGYM', 'HGYM', 'HGYM', 'HGYM', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'PATH', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'PATH', 'STAI', 'STAI' ],
+[ 'CLAS', 'CLAS', 'PATH', 'PATH', 'WALL', 'HGYM', 'HGYM', 'HGYM', 'HGYM', 'HGYM', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'OUTD' ],
+[ 'CLAS', 'CLAS', 'WALL', 'WALL', 'WALL', 'HGYM', 'HGYM', 'HGYM', 'HGYM', 'HGYM', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'PATH', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'PATH', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'PATH', 'WALL', 'WALL', 'WALL' ],
+[ 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'HGYM', 'HGYM', 'HGYM', 'HGYM', 'HGYM', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'PATH', 'COMM', 'WALL', 'BAND', 'BAND', 'BAND', 'BAND', 'BAND', 'WALL', 'WALL', 'WALL', 'WALL', 'PATH', 'PATH', 'CLAS', 'CLAS' ],
+[ 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'HGYM', 'HGYM', 'HGYM', 'HGYM', 'HGYM', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'PATH', 'COMM', 'WALL', 'BAND', 'BAND', 'BAND', 'BAND', 'BAND', 'WALL', 'WALL', 'WALL', 'WALL', 'PATH', 'WALL', 'CLAS', 'CLAS' ], 
+[ 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'HGYM', 'HGYM', 'HGYM', 'HGYM', 'HGYM', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'PATH', 'COMM', 'WALL', 'BAND', 'BAND', 'BAND', 'BAND', 'BAND', 'WALL', 'WALL', 'WALL', 'WALL', 'PATH', 'WALL', 'WALL', 'WALL' ],
+[ 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'PATH', 'COMM', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'PATH', 'WALL', 'WALL', 'WALL', 'WALL', 'PATH', 'PATH', 'CLAS', 'CLAS' ],
+[ 'WALL', 'WALL', 'WALL', 'WALL', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'COMM', 'WALL', 'LGI ', 'LGI ', 'LGI ', 'LGI ', 'PATH', 'PATH', 'WALL', 'WALL', 'WALL', 'PATH', 'WALL', 'WALL', 'WALL' ],
+[ 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'STAI', 'STAI', 'WALL', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'COMM', 'WALL', 'LGI ', 'LGI ', 'LGI ', 'LGI ', 'WALL', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'CLAS', 'CLAS' ],
+[ 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'PATH', 'COMM', 'WALL', 'LGI ', 'LGI ', 'LGI ', 'LGI ', 'WALL', 'CLAS', 'WALL', 'CLAS', 'WALL', 'PATH', 'WALL', 'WALL', 'WALL' ],
+[ 'MGYM', 'MGYM', 'MGYM', 'MGYM', 'MGYM', 'MGYM', 'WALL', 'WALL', 'LIBR', 'LIBR', 'LIBR', 'LIBR', 'LIBR', 'LIBR', 'LIBR', 'WALL', 'WALL', 'PATH', 'COMM', 'WALL', 'LGI ', 'LGI ', 'LGI ', 'LGI ', 'WALL', 'CLAS', 'WALL', 'CLAS', 'WALL', 'PATH', 'WALL', 'CLAS', 'CLAS' ],
+[ 'MGYM', 'MGYM', 'MGYM', 'MGYM', 'MGYM', 'MGYM', 'WALL', 'WALL', 'LIBR', 'LIBR', 'LIBR', 'LIBR', 'LIBR', 'LIBR', 'LIBR', 'WALL', 'CLAS', 'PATH', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'PATH', 'WALL', 'PATH', 'WALL', 'PATH', 'WALL', 'PATH', 'WALL', 'PATH', 'WALL' ],  
+[ 'MGYM', 'MGYM', 'MGYM', 'MGYM', 'MGYM', 'MGYM', 'WALL', 'WALL', 'LIBR', 'LIBR', 'LIBR', 'LIBR', 'LIBR', 'LIBR', 'LIBR', 'WALL', 'WALL', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'STAI' ],
+[ 'MGYM', 'MGYM', 'MGYM', 'MGYM', 'MGYM', 'MGYM', 'WALL', 'WALL', 'LIBR', 'LIBR', 'LIBR', 'LIBR', 'LIBR', 'LIBR', 'LIBR', 'WALL', 'WALL', 'WALL', 'PATH', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL' ],
+[ 'MGYM', 'MGYM', 'MGYM', 'MGYM', 'MGYM', 'MGYM', 'WALL', 'WALL', 'LIBR', 'LIBR', 'LIBR', 'LIBR', 'LIBR', 'LIBR', 'LIBR', 'WALL', 'WALL', 'WALL', 'PATH', 'STAI', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL' ], 
+[ 'MGYM', 'MGYM', 'MGYM', 'MGYM', 'MGYM', 'MGYM', 'WALL', 'WALL', 'LIBR', 'LIBR', 'LIBR', 'LIBR', 'LIBR', 'LIBR', 'LIBR', 'LIBR', 'LIBR', 'WALL', 'PATH', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'PATH', 'WALL', 'WALL', 'WALL' ],
+[ 'WALL', 'WALL', 'WALL', 'PATH', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'PATH', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'PATH', 'WALL', 'NURS', 'NURS', 'NURS', 'WALL', 'AUDI', 'AUDI', 'AUDI', 'AUDI', 'AUDI', 'AUDI', 'WALL', 'STAG', 'STAG' ],
+[ 'PATH', 'PATH', 'PATH', 'PATH', 'WALL', 'CLAS', 'CLAS', 'CLAS', 'WALL', 'WALL', 'PATH', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'PATH', 'WALL', 'PATH', 'WALL', 'WALL', 'WALL', 'AUDI', 'AUDI', 'AUDI', 'AUDI', 'AUDI', 'AUDI', 'WALL', 'STAG', 'STAG' ],
+[ 'WALL', 'WALL', 'WALL', 'PATH', 'WALL', 'PATH', 'WALL', 'WALL', 'WALL', 'WALL', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'AUDI', 'AUDI', 'AUDI', 'AUDI', 'AUDI', 'AUDI', 'WALL', 'STAG', 'STAG' ],
+[ 'CLAS', 'CLAS', 'WALL', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'PATH', 'WALL', 'WALL', 'WALL', 'PATH', 'PATH', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL' ],
+[ 'CLAS', 'CLAS', 'PATH', 'PATH', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'PATH', 'PATH', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL' ],
+[ 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'OUTD', 'OUTD', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL', 'WALL' ]
+];
+
+
+module.exports = FIRST_FLOOR;
