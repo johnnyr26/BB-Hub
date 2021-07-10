7c8c875f1516f250f9ffba5f6ef19a18d17cfe25
Johnny Ramirez <ramirezjohnny@icloud.com>
2021-07-10 17:55:22 -0400

    Change Lunch API link
    This link provides more concise data
    
    Doesn't deal with not knowing when school is open/closed

--- controllers/lunch.js
+++ controllers/lunch.js
@@ -1,27 +1,11 @@
 const axios = require('axios');
 
 module.exports = async (req, res) => {
-    const response = await axios.get('https://apiservicelocators.fdmealplanner.com/api/v1/data-locator-webapi/3/meals?menuId=0&accountId=35&locationId=119&mealPeriodId=2&monthId=6&timeOffset=300&_=1625800627442');
+    const response = await axios.get('https://api.getchoosi.com/KidsChooseApi/v1.0/menus?filter%5Bdate%5D=5-12-2021&filter%5Bschool%5D=a0f85ce7-e126-497b-9633-ebf677ea68ae');
     const meals = [];
-    let valuesFor15th = [];
-    let valuesFor16th = [];
-    response.data.result.forEach((data, index) => {
-        if (!data.menuRecipiesData) {
-            return;
-        }
-        const foods = [];
-        data.menuRecipiesData.forEach(meal => {
-            const lunchIsServed = meal.isShowOnMenu === 1 || foods.length > 0;
-            if (lunchIsServed) {
-                foods.push(meal.componentEnglishName);
-            }
-        });
-        if (foods.length > 0) {
-            meals.push({
-                meal: foods,
-                date: data.strMenuForDate
-            });
-        }
+    response.data.menu.grabNGoItems.forEach(item => {
+        const { name, description } = item;
+        meals.push({ name, description})
     });
     return res.render('lunch', { meals });
 };
