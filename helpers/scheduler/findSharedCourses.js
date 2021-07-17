e2cf38454e4d287f5e2dbae1d13388b96ee0c8ee
Johnny Ramirez <ramirezjohnny@icloud.com>
2021-07-17 12:33:11 -0400

    Add second floor svg

--- helpers/scheduler/findSharedCourses.js
+++ helpers/scheduler/findSharedCourses.js
@@ -1,16 +1,19 @@
 module.exports = (schedule1, schedule2) => {
     const sharedCourses = [];
+
     schedule1.forEach(course1 => {
         schedule2.forEach(course2 => {
             const sameCourseTitle = course1.name === course2.name;
             const samePeriods = course1.period === course2.period;
             const sameTeacher = course1.teacher === course2.teacher; 
             const sameLetterDays = course1.letterDays.every(letterDay => course2.letterDays.includes(letterDay));
+
             if (sameCourseTitle && samePeriods && sameTeacher && sameLetterDays) {
                 sharedCourses.push(course1);
             }
         });
     });
+    
     return sharedCourses;
 }
 
