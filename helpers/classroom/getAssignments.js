2680d5a989d5ebb38647b11bfc05f62d7c9e04a4
Johnny Ramirez <ramirezjohnny@icloud.com>
2021-08-01 15:44:57 -0400

    Make friends row multiple rows

--- helpers/classroom/getAssignments.js
+++ helpers/classroom/getAssignments.js
@@ -110,6 +110,7 @@ module.exports = async (req, res) => {
                         courseWorkId,
                         states: ['CREATED', 'TURNED_IN', 'RECLAIMED_BY_STUDENT']
                     });
+                    console.log(submissionResponse);
                     const state = submissionResponse.data.studentSubmissions[0].state;
                     
                     assignments.push({
