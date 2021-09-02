22e0b59c4bf6e32d778a061ec6f0fa78cd5a24cc
Johnny Ramirez <ramirezjohnny@icloud.com>
2021-09-01 20:58:00 -0400

    Show confirmed games, not past gamees

--- helpers/sports/getSportGames.js
+++ helpers/sports/getSportGames.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 module.exports = async () => {
     const response = await axios.get('https://calendar.instone.com/get.php?cal=f83bf7c0-6eaa-11e4-a95f-fefd42e427e7&offset=0&limit=1000');
-    return response.data.filter(event => event.teamNames[0] !== 'M').map(event => {
+    return response.data.filter(event => event.teamNames[0] !== 'M' && event.Status === 'Confirmed').map(event => {
         return {
             sport: event.teamNames,
             opponent: event.Opponent,
