4be8e76344cfe92c88b9859f910200a0b905e944
Johnny Ramirez <ramirezjohnny@icloud.com>
2022-01-09 23:43:28 -0500

    Revert schedule to normal

--- assets/highSchoolSchedule.js
+++ assets/highSchoolSchedule.js
@@ -9,7 +9,7 @@ const SCHEDULE_STRUCTURE = {
     H: [5, 6, 7, 8, 1, 2]
 }
 
-const HIGH_SCHOOL_SCHEDULE = {
+const TWO_HOUR_DELAY_HIGH_SCHOOL_SCHEDULE = {
     schedule: SCHEDULE_STRUCTURE,
     times:[
         {
@@ -46,6 +46,47 @@ const HIGH_SCHOOL_SCHEDULE = {
             block: 6,
             from: "14:02",
             to: "14:40"
+        }
+    ]
+};
+
+const HIGH_SCHOOL_SCHEDULE = {
+    schedule: SCHEDULE_STRUCTURE,
+    times:[
+        {
+            block: 1,
+            from: "07:50",
+            to: "08:43"
+        },
+        {
+            block: 2,
+            from: "08:48",
+            to: "09:41"
+        },
+        {
+            block: 3,
+            from: "09:46",
+            to: "10:39"
+        },
+        {
+            block: "LUNCH",
+            from: "10:42",
+            to: "11:28"
+        },
+        {
+            block: 4,
+            from: "11:31",
+            to: "12:24"
+        },
+        {
+            block: 5,
+            from: "12:29",
+            to: "13:22"
+        },
+        {
+            block: 6,
+            from: "13:27",
+            to: "14:20"
         }
     ]
 };
